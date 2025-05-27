79165404c0b2864087337324f4ee54209a1a1181
Wendel <wendeljunior1527@gmail.com>
2025-05-27 20:32:48 -0300

    Upei arquivo txt

--- ForcaMain.c
+++ ForcaMain.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Functions.h"
 
 int main() {
-pritf("Hello World!");
+    int opcao;
+    FILE *arquivo;
+    char linha[256];
+
+    arquivo = fopen("Opcoes_jogo_da_forca.txt", "r");
+
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    while (fgets(linha, sizeof(linha), arquivo)) {
+        printf("%s", linha); 
+    }
+    
+    fclose(arquivo);
+
+    do {
+        printf("\n| BEM VINDO AO JOGO DA FORCA:\n");
+        Opcoes_Jogo();
+        printf("Digite sua opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+
+            break;
+
+            default:
+            printf("Opcao invalida. Por favor, tente novamente.\n");
+        }
+    } while(opcao != 0);
 
 return 0;
 }
